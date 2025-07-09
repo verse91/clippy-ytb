a5f408187fc2260884adc5f6f39d44f2f4a17704
VERSE <sunnyone0901@gmail.com>
2025-07-09 09:40:55 +0700

    feat: integrate Radix UI components for sign-in modal and update dependencies

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,15 +1,9 @@
-"use client"
-import React, { useState } from "react";
-// Import your LoginForm component
-import { LoginForm }     from "@/components/login-form"; // Adjust the path as needed
+"use client";
+import React from "react";
+import SignInModal from "@/components/login-form";
 import { Button } from "./ui/button";
 
 export default function Navbar() {
-  const [showLogin, setShowLogin] = useState(false);
-
-  const handleOpenLogin = () => setShowLogin(true);
-  const handleCloseLogin = () => setShowLogin(false);
-
   return (
     <>
       <nav className="absolute top-0 left-0 w-full flex items-center justify-between px-8 py-4 z-10">
@@ -33,31 +27,22 @@ export default function Navbar() {
             href="https://github.com/verse91/clippy-ytb"
             target="_blank"
             rel="noopener noreferrer"
+            title="Star it on GitHub ⭐"
+            className="transition-colors group"
           >
-            <i className="bxl  bx-github text-4xl" style={{color:"#fff"}}></i>
-          </a>
-          <button className="p-3 cursor-pointer" onClick={handleOpenLogin}>
             <i
-              className="bxr bxs-user text-3xl"
-              style={{ color: "#ffffff" }}
+              className="bxl bx-github text-4xl text-white transition-all group-hover:text-gray-300 group-hover:scale-105"
             ></i>
-          </button>
+          </a>
+          <SignInModal
+            trigger={
+              <button className="p-3 cursor-pointer">
+                <i className="bx bxs-user text-3xl text-white hover:text-gray-300 transition-all hover:scale-105"></i>
+              </button>
+            }
+          />
         </div>
       </nav>
-      {showLogin && (
-        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50">
-          <div>
-            <button
-              className="absolute top-2 right-2 text-gray-500 hover:text-gray-700"
-              onClick={handleCloseLogin}
-              aria-label="Close"
-            >
-              &times;
-            </button>
-            <LoginForm />
-          </div>
-        </div>
-      )}
     </>
   );
 }
